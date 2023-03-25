183f2bd89798bbb6bc08b75394d9047eb332051b
Martín Javier <martinjavierd@gmail.com>
2023-03-25 13:41:48 +0100

    integrando el chat

--- src/app.js
+++ src/app.js
@@ -36,6 +36,37 @@ const httpServer = app.listen(8080, () => {
 
 const socketServer = new Server(httpServer);
 
+/*
+const chatInput = document.getElementById("chat-input");
+
+chatInput.addEventListener("keyup", (ev) => {
+  if (ev.key === "Enter") {
+    const inputMessage = chatInput.value;
+    if (inputMessage.trim().length > 0) {
+      let timestamp = new Date().toISOString();
+      socket.emit("chat-message", {
+        timestamp,
+        username,
+        message: inputMessage,
+      });
+    }
+  }
+});
+*/
+
+/*
+const messageElement = document.getElementById("messages-panel");
+socket.on("messages", (data) => {
+  let messages = "";
+
+  data.forEach((m) => {
+    messages += `<b>(${m.timestamp})</b> <b>${m.username}:</b> ${m.message}</br>`;
+  });
+
+  messageElement.innerHTML = messages;
+});
+*/
+
 socketServer.on("connection", (socket) => {
   console.log("New client connected!");
 
